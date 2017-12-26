af0c6f9df2502cb93d59211f90994a9ff3685a3b
Jacob Henry <jakehenry.tn@gmail.com>
2017-12-26 16:14:48 -0700

    Switched to polybar

--- .config/lemonbar/mylemon/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ~/.config/lemonbar/mylemon
-[ ! -e venv ] && virtualenv -q venv 
-source ./venv/bin/activate
-pip install -q -r ./requirements.txt
-
-python ./my_lemon.py | lemonbar -p -b -f Inconsolata -f Steel_Alphabet -g x30++ -u 2 -o -3 | zsh
-
